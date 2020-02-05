927d1ffe23b0c3210466bd028a557520c45d0bce
Bas <Bas.ghosts@gmail.com>
2020-02-05 13:59:10 +0100

    TextMeshPro / Post Processing / Iinteractable interface and menu base

---
+++ PlanetEchoProjectContext2020/Assets/_Scripts/WorldButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Bas
+{
+    public class WorldButton : MonoBehaviour, Iinteractable
+    {
+        public UnityEvent OnClick = new UnityEvent();
+        private GameObject definedButton;
+
+        void Start()
+        {
+            definedButton = this.gameObject;
+        }
+
+        public void Interact()
+        {
+            Debug.Log("Interacting with: " + this.gameObject.name);
+            OnClick.Invoke();
+        }
+    }
+}
