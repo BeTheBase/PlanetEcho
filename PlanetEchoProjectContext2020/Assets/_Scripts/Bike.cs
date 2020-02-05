e9262975ed9218ba732e058a5d4bd2bce1de2984
Bas <Bas.ghosts@gmail.com>
2020-02-05 16:07:00 +0100

    HDRP merging

---
+++ PlanetEchoProjectContext2020/Assets/_Scripts/Bike.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour
+{
+    public Transform EndPosition;
+
+    public float Speed = 10f;
+
+    public Transform startPosition;
+    public Vector3 EndVector;
+
+    private float timer = 0;
+
+    private void Start()
+    {
+        startPosition = this.transform;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        transform.position = Vector3.Lerp(startPosition.position, EndPosition.position, Mathf.PingPong(timer * Speed, 1.0f));
+    }
+}
