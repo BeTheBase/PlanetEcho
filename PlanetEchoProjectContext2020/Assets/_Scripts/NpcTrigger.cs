ae73af1fadad8eb7b3580b0550a579f5bac41baf
Bas <Bas.ghosts@gmail.com>
2020-02-06 17:14:21 +0100

    POST PROCESSING FIXED

---
+++ PlanetEchoProjectContext2020/Assets/_Scripts/NpcTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bas
+{
+    public class NpcTrigger : MonoBehaviour
+    {
+        public int DialogueSequenceID = 0;
+        public string DialogueLineName = string.Empty;
+        public float PlayerFreezeTime = 10f;
+        public void TriggerDialogue()
+        {
+            DialogueManager.Instance.GetDialogueLineBySeqeuenceID(DialogueSequenceID, DialogueLineName);
+        }
+    }
+}
---
+++ PlanetEchoProjectContext2020/Assets/_Scripts/PlayerDetect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ruben;
+
+namespace Bas
+{
+    public class PlayerDetect : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.HasComponent<NpcTrigger>())
+            {
+                var trigger = other.gameObject.GetComponent<NpcTrigger>();
+                trigger.TriggerDialogue();
+                MonoBehaviour playerMovment = this.gameObject.GetComponent<MovementController>();
+                playerMovment.enabled = false;
+                StartCoroutine(ActivateBehaviour(playerMovment, trigger.PlayerFreezeTime));
+            }
+        }
+
+        private IEnumerator ActivateBehaviour(MonoBehaviour behaviour, float time)
+        {
+            yield return new WaitForSeconds(time);
+            behaviour.enabled = true;
+        }
+    }
+}
