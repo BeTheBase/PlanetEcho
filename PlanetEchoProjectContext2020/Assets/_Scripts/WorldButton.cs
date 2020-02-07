ccba5d3d21aebd1e1b414f13162b734c5743cbd7
Bas <Bas.ghosts@gmail.com>
2020-02-07 14:20:05 +0100

    Planet Hover

--- PlanetEchoProjectContext2020/Assets/_Scripts/WorldButton.cs
+++ PlanetEchoProjectContext2020/Assets/_Scripts/WorldButton.cs
@@ -7,8 +7,23 @@ namespace Bas
 {
     public class WorldButton : MonoBehaviour, Iinteractable
     {
+        public float HoverSize = 0.75f;
         public UnityEvent OnClick = new UnityEvent();
         private GameObject definedButton;
+
+        void OnMouseEnter()
+        {
+            //If your mouse hovers over the GameObject with the script attached, output this message
+            Debug.Log("Mouse is over GameObject.");
+            transform.localScale += Vector3.one * HoverSize;
+        }
+
+        void OnMouseExit()
+        {
+            //The mouse is no longer hovering over the GameObject so output this message each frame
+            Debug.Log("Mouse is no longer on GameObject.");
+            transform.localScale -= Vector3.one * HoverSize;
+        }
 
         void Start()
         {
